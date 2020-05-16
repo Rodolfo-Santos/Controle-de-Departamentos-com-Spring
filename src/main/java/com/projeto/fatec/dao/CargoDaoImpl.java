483a4fcf7a341ca52ffc1a03d5103e0a13b8cf17
Rodolfo Santos <rodolfodossantos29@gmail.com>
2020-05-16 18:49:25 -0300

    Subindo versão de backup

---
+++ src/main/java/com/projeto/fatec/dao/CargoDaoImpl.java
@@ -0,0 +1,11 @@
+package com.projeto.fatec.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.projeto.fatec.domain.Cargo;
+
+@Repository
+public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao {
+
+	
+}
---
+++ src/main/java/com/projeto/fatec/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,10 @@
+package com.projeto.fatec.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.projeto.fatec.domain.Departamento;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao{
+	
+}
